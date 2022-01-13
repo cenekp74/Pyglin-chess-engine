e168d6428fdbc743d9a28c1736b2169e2916cfcc
Cenek Potuznik <81622186+cenekp74@users.noreply.github.com>
2022-01-13 15:14:33 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 import chess
 import time
 import requests
-import random
 
 PIECES_VAL = {'p': 100, 'n': 320, 'b': 330, 'r': 500, 'q': 900, 'k': 0}
 PIECES_SQUARES_W = {
     'p': [
         0,  0,  0,  0,  0,  0,  0,  0,
         5, 10, 9, -23, -23, 9, 10,  5,
-        5, -5, -10,  0,  0, -10, -5,  5,
+        8, -5, -10,  0,  0, -10, -5,  8,
         0,  0,  0,  22, 22,  0,  0,  0,
         5,  5, 10, 26, 26, 10,  5,  5,
         10, 10, 20, 30, 30, 20, 10, 10,
@@ -36,7 +35,7 @@ PIECES_SQUARES_W = {
         -20, -10, -10, -10, -10, -10, -10, -20
     ],
     'r': [
-        0, 0, 0, 5, 5, 0, 0, 0,
+        -5, -5, 0, 5, 5, -1, -5, -5,
         -5, 0, 0, 0, 0, 0, 0, -5,
         -5, 0, 0, 0, 0, 0, 0, -5,
         -5, 0, 0, 0, 0, 0, 0, -5,
@@ -56,9 +55,9 @@ PIECES_SQUARES_W = {
         -20, -10, -10, -5, -5, -10, -10, -20
     ],
 	'k': [
-        20, 40, 10, 0, 0, 20, 35, 20,
-        20, 20, 0, 0, 0, 0, 20, 20,
-        -10, -20, -20, -20, -20, -20, -20, -10,
+        15, 40, 5, 0, 0, 30, 35, 15,
+        8, 10, -10, -10, -10, -10, 10, 8,
+        -15, -25, -30, -30, -30, -30, -25, -15,
         20, -30, -30, -40, -40, -30, -30, -20,
         -30, -40, -40, -50, -50, -40, -40, -30,
         -30, -40, -40, -50, -50, -40, -40, -30,
@@ -66,8 +65,6 @@ PIECES_SQUARES_W = {
         -30, -40, -40, -50, -50, -40, -40, -30
     ]
 }
-
-max_tt = 1e5
 
 PIECES_SQUARES_B = {
     'p': list(reversed(PIECES_SQUARES_W['p'])),
@@ -81,12 +78,12 @@ PIECES_SQUARES_B = {
 PIECES_SQUARES_W_ENDGAME = {
     'p': [
         0,  0,  0,  0,  0,  0,  0,  0,
-        -10, -10, -10, -15, -15, -10, -10,  -10,
-        -5, -5, -5,  -1,  -1, -5, -5,  5,
-        0,  0,  0, 5, 5,  0,  0,  0,
-        15,  15, 15, 20, 20, 15,  15,  15,
-        30, 30, 30, 32, 32, 30, 30, 30,
+        -25, -25, -25, -25, -25, -25, -25,  -25,
+        -15, -15, -15,  -15,  -15, -15, -15,  15,
+        -5, -5,  -5, -5, -5,  -5,  -5,  -5,
+        30,  30, 30, 30, 30, 30,  30,  30,
         50, 50, 50, 50, 50, 50, 50, 50,
+        70, 70, 70, 70, 70, 70, 70, 70,
         0, 0, 0, 0, 0, 0, 0, 0
     ],
     'n': [
@@ -150,25 +147,75 @@ PIECES_SQUARES_B_ENDGAME = {
     'k': list(reversed(PIECES_SQUARES_W_ENDGAME['k']))
 }
 
+max_tt = 1e5
+
 tt = {}
+
+def evaluate(board):
+    eval = 0
+    isEndgame = is_endgame(board)
+    outcome = board.outcome()
+    if outcome != None:
+        if outcome.termination == chess.Termination.CHECKMATE:
+            return 10000 if outcome.winner else -10000
+    if board.is_stalemate() or board.is_repetition():
+        return 0
+    pawnsW = {0:0, 1:0, 2:0, 3:0, 4:0, 5:0, 6:0, 7:0}
+    pawnsB = {0:0, 1:0, 2:0, 3:0, 4:0, 5:0, 6:0, 7:0}
+    whiteKingSq = board.king(chess.WHITE)
+    blackKingSq = board.king(chess.BLACK)
+    if isEndgame:
+        for piecetype, value in PIECES_VAL.items():
+            for wPieceSq in board.pieces(chess.PIECE_SYMBOLS.index(piecetype), 1):
+                eval += value + PIECES_SQUARES_W_ENDGAME[piecetype][wPieceSq]
+                if piecetype == 'p':
+                    file = chess.square_file(wPieceSq)
+                    pawnsW[file] += 1
+                    if pawnsW[file] > 1:
+                        eval -= 22
+                    if chess.square_distance(wPieceSq, whiteKingSq) <= 2:
+                        eval += 7
+            for bPieceSq in board.pieces(chess.PIECE_SYMBOLS.index(piecetype), 0):
+                eval -= value + PIECES_SQUARES_B_ENDGAME[piecetype][bPieceSq]
+                if piecetype == 'p':
+                    file = chess.square_file(bPieceSq)
+                    pawnsB[file] += 1
+                    if pawnsB[file] > 1:
+                        eval += 22
+                    if chess.square_distance(bPieceSq, blackKingSq) <= 2:
+                        eval -= 7
+    else:
+        for piecetype, value in PIECES_VAL.items():
+            for wPieceSq in board.pieces(chess.PIECE_SYMBOLS.index(piecetype), 1):
+                eval += value + PIECES_SQUARES_W[piecetype][wPieceSq]
+                if piecetype == 'p':
+                    file = chess.square_file(wPieceSq)
+                    pawnsW[file] += 1
+                    if pawnsW[file] > 1:
+                        eval -= 22
+                    if chess.square_distance(wPieceSq, whiteKingSq) <= 2:
+                        eval += 7
+            for bPieceSq in board.pieces(chess.PIECE_SYMBOLS.index(piecetype), 0):
+                eval -= value + PIECES_SQUARES_B[piecetype][bPieceSq]
+                if piecetype == 'p':
+                    file = chess.square_file(bPieceSq)
+                    pawnsB[file] += 1
+                    if pawnsB[file] > 1:
+                        eval += 22
+                    if chess.square_distance(bPieceSq, blackKingSq) <= 2:
+                        eval -= 7
+
+    return eval * 1 if board.turn else eval * -1
 
 def quiesce(board, alpha, beta, searchN):
     eval = evaluate(board)
-    if searchN > 50:
-        print(searchN)
-        return eval
     if eval >= beta:
         return beta
     if alpha < eval:
         alpha = eval
-    if board.is_checkmate() or board.is_repetition() or board.is_stalemate():
-        return eval
-    for move in list(board.legal_moves):
-        if searchN > 50:
-            print(searchN)
-            return eval
+    for move in sort_moves(board):
         if (board.is_capture(move) and not board.is_en_passant(move)):
-            if ((PIECES_VAL[board.piece_at(move.to_square).symbol().lower()] - PIECES_VAL[board.piece_at(move.from_square).symbol().lower()]) > 100) or not board.is_attacked_by(not board.turn, move.to_square):
+            if PIECES_VAL[board.piece_at(move.to_square).symbol().lower()] >= PIECES_VAL[board.piece_at(move.from_square).symbol().lower()] or not board.is_attacked_by(not board.turn, move.to_square):
                 board.push(move)
                 searchN += 1
                 score = -quiesce(board, -beta, -alpha, searchN)
@@ -177,7 +224,16 @@ def quiesce(board, alpha, beta, searchN):
                     return beta
                 if score > alpha:
                     alpha = score
-        if board.gives_check(move):
+            elif board.gives_check(move):
+                board.push(move)
+                searchN += 1
+                score = -quiesce(board, -beta, -alpha, searchN)
+                board.pop()
+                if score >= beta:
+                    return beta
+                if score > alpha:
+                    alpha = score
+        elif board.gives_check(move):
             board.push(move)
             searchN += 1
             score = -quiesce(board, -beta, -alpha, searchN)
@@ -188,36 +244,9 @@ def quiesce(board, alpha, beta, searchN):
                 alpha = score
     return alpha
 
-
-def evaluate(board):
-    eval = 0
-    isEndgame = is_endgame(board)
-    if board.is_checkmate():
-        if board.turn:
-            return -10000
-        else:
-            return 10000
-    if board.is_stalemate() or board.is_fivefold_repetition() or board.is_repetition():
-        return 0
-
-    if isEndgame:
-        for piecetype, value in PIECES_VAL.items():
-            for wPieceSq in board.pieces(chess.PIECE_SYMBOLS.index(piecetype), 1):
-                eval += value + PIECES_SQUARES_W_ENDGAME[piecetype][wPieceSq]
-            for bPieceSq in board.pieces(chess.PIECE_SYMBOLS.index(piecetype), 0):
-                eval -= value + PIECES_SQUARES_B_ENDGAME[piecetype][bPieceSq]
-    else:
-        for piecetype, value in PIECES_VAL.items():
-            for wPieceSq in board.pieces(chess.PIECE_SYMBOLS.index(piecetype), 1):
-                eval += value + PIECES_SQUARES_W[piecetype][wPieceSq]
-            for bPieceSq in board.pieces(chess.PIECE_SYMBOLS.index(piecetype), 0):
-                eval -= value + PIECES_SQUARES_B[piecetype][bPieceSq]
-
-    return eval * 1 if board.turn else eval * -1
-
-
 def sort_moves(board):
     remaining = list(board.legal_moves)
+
     t = tt.get(board.fen())
     if t:
         best = t[2]
@@ -245,6 +274,7 @@ def sort_moves(board):
         yield move
 
 times = []
+color = {'self':None}
 
 def search(board, depth):
     result = pvs(board, -10000, 10000, depth)
@@ -254,14 +284,15 @@ def search(board, depth):
 
 def find_move(board, depth):
     start = time.time()
+    color['self'] = not board.turn
     if board.ply() < 30:
         response = requests.get(f"https://explorer.lichess.ovh/masters?fen={board.fen()}")
         json = response.json()
-        if len(json['moves']) > 1:
-            moveUci = (json['moves'][random.randint(0, 1)])['uci']
+        if len(json['moves']) > 0:
+            moveUci = (json['moves'][0])['uci']
             move = chess.Move.from_uci(moveUci)
             return move
-    move = search(board, depth)[0]
+    move, eval = search(board, depth)
 
 
     times.append(time.time() - start)
@@ -272,6 +303,7 @@ def find_move(board, depth):
     if move == None:
         print('None')
         return list(board.legal_moves)[0]
+    print(eval)
     return move
 
 def is_endgame(board):
@@ -279,17 +311,21 @@ def is_endgame(board):
     for piecetype, value in PIECES_VAL.items():
         for wPieceSq in board.pieces(chess.PIECE_SYMBOLS.index(piecetype), 1):
             material += value
+            if piecetype == 'q':
+                material += 100
         for bPieceSq in board.pieces(chess.PIECE_SYMBOLS.index(piecetype), 0):
             material += value
-    if material <= 4800:
+            if piecetype == 'q':
+                material += 100
+    if material <= 4000:
         return True
     else:
         return False
 
 
 def pvs(board, alpha, beta, depthLeft):
-    if board.is_checkmate() or board.is_stalemate() or board.is_repetition():
-        return -evaluate(board), None
+    if board.is_game_over():
+        return evaluate(board), None
     if depthLeft == 0:
         return -quiesce(board, alpha, beta, 0), None
     t = tt.get(board.fen())
@@ -316,8 +352,8 @@ def pvs(board, alpha, beta, depthLeft):
     return -alpha, best
 
 def zws(board, beta, depthLeft):
-    if board.is_checkmate() or board.is_stalemate() or board.is_repetition():
-        return -evaluate(board)
+    if board.is_game_over():
+        return evaluate(board)
     if depthLeft == 0:
         return quiesce(board, beta-1, beta, 0)
     for move in sort_moves(board):
@@ -327,9 +363,3 @@ def zws(board, beta, depthLeft):
         if score >= beta:
             return beta
     return beta-1
-
-if __name__ == '__main__':
-    board = chess.Board('rnbqkbnr/pppppppp/8/7Q/2B1P3/8/PPPP1PPP/RNB1K1NR b KQkq - 0 1')
-    start = time.time()
-    print(find_move(board, 3))
-    print(time.time() - start)
